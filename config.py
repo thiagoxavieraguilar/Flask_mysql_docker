228a6e1f37f280c561d7dab27404b9c9eb902082
Thiago Xavier <thiago.xavieraguilar1@gmail.com>
2022-12-08 00:23:28 -0300

    db

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from run import app
 
-engine = create_engine('mysql+pymysql://root:root@localhost')
-conn = engine.connect()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql://root:root@localhost/cadastros')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Accounts(db.Model):
+    email = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
+    
+with app.app_context():
+    db.create_all()
--- run.py
+++ run.py
@@ -1,42 +1,35 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template,request,redirect,url_for,flash
 
 app = Flask(__name__)
 
-
+@app.route('/', methods=['GET','POST'])
 @app.route('/login', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['pwd']
-        return show(email,pwd)
-        #return redirect(url_for('show',email=email,password=password))
-
+        return show(email,password)
     return render_template('login.html')
 
 @app.route('/show', methods=['GET','POST'])
-def show(email:str,password:str,confirm_password=None):
-    return render_template('show.html',email=email,password=password,confirm_password=confirm_password)
+def show(email:str,password:str):
+    return render_template('show.html',email=email,password=password)
 
 @app.route('/creat_account', methods=['GET','POST'])
 def creat_account():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['pwd']
-        #confirm_password = request.form['cpwd']
-        #if password == confirm_password:
-        return show(email,password)
-        #else:
-        #return('verifique se a senha está correta')
-        #return redirect(url_for('show'(email,password,confirm_password)))
-    return('olá')
-    #return render_template('create_account.html')
+        confirm_password = request.form['cpwd']
+        if password == confirm_password:
+            #cria a conta
+            flash('Conta criada com sucesso')
+            return redirect(url_for(login))
+        else:
+            flash('Verifique se a senha está correta')
+    return render_template('create_account.html')
 
-@app.route('/')
-def hello():
-    return 'hello from docker'
-
-#,port=80
-    
+  
 if __name__ == '__main__':
     app.run(debug=True)
 
